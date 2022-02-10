3b9c1bd0f7eb3df5bbb063c2a6df731af6efe812
bilsilver99 <34319555+bilsilver99@users.noreply.github.com>
2022-02-10 09:36:13 -0500

    part one

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
+    "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
   ],
 };
